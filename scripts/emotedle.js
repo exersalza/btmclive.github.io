52d4d5dc18936bb6c59af83daba7609cc3f6cba5
exersalza "Julian J. <exersalza.dev@gmail.com>
2025-10-16 06:28:25 +0200

    Fix: typo

--- scripts/emotedle.js
+++ scripts/emotedle.js
@@ -141,7 +141,7 @@ window.addEventListener("DOMContentLoaded", async () => {
         let img = new Image();
         let tmp = document.createElement('p');
         document.getElementById('emote-display').appendChild(tmp).innerHTML = "Loading..";
-        const res = await fetch(url + attempt, { cache: "no-csche" });
+        const res = await fetch(url + attempt, { cache: "no-cache" });
         try {
             if (!res.ok) throw new Error('HTTP ' + res.status);
             let e = url + attempt;
