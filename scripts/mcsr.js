7b7b41248c0bee80561f86b146b421d1b9aaa9e2
kamythol <reaml@tuta.io>
2026-01-09 04:50:03 -0800

    mcsr: add wins by forfeits

--- scripts/mcsr.js
+++ scripts/mcsr.js
@@ -63,11 +63,13 @@ async function showInfo() {
     | <span style="color: #60a5fa">${data.today.draws}D</span>
     | ${data.today.forfeits}FFs`;
     document.getElementById("net-elo").innerHTML = `${elo_today} elo`;
+    document.getElementById("ff-wins-today").innerHTML = `${data.today.forfeit_wins} wins by forfeits`;
     // -- season -- //
     document.getElementById("peak-elo-season").innerHTML = `Peak elo: <span class="${getRank(data.season.elo_peak).class}">${data.season.elo_peak} (${getRank(data.season.elo_peak).short})</span>`;
     document.getElementById("lowest-elo-season").innerHTML = `Lowest elo: <span class="${getRank(data.season.elo_lowest).class}">${data.season.elo_lowest} (${getRank(data.season.elo_lowest).short})</span>`;
     document.getElementById("pb-season").innerHTML = `PB: ${data.season.pb}`;
     document.getElementById("forfeits-season").innerHTML = `Forfeits: ${data.season.forfeits}`;
+    document.getElementById("ff-wins-season").innerHTML = `${data.season.forfeit_wins} wins by forfeits`;
     // -- overall -- //
     document.getElementById("pb-overall").innerHTML = `PB: ${data.overall.pb}`;
     document.getElementById("peak-elo-overall").innerHTML = `Peak elo: <span class="${getRank(data.overall.elo_peak).class}">${data.overall.elo_peak} (${getRank(data.overall.elo_peak).short})</span>`;
